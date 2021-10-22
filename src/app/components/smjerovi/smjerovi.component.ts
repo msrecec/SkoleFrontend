9b5cc17334c419e25885cb70dcfad99d99c3ea01
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-22 10:26:07 +0200

    added smjerovi list

--- src/app/components/smjerovi/smjerovi.component.ts
+++ src/app/components/smjerovi/smjerovi.component.ts
@@ -1,15 +1,30 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data, Router } from '@angular/router';
+import { Nastavnik } from 'src/app/model/nastavnik/nastavnik-model';
+import { Smjer } from 'src/app/model/smjer/smjer-model';
 
 @Component({
   selector: 'app-smjerovi',
   templateUrl: './smjerovi.component.html',
-  styleUrls: ['./smjerovi.component.css']
+  styleUrls: ['./smjerovi.component.css'],
 })
 export class SmjeroviComponent implements OnInit {
+  smjerovi!: Smjer[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private _location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.route.data.subscribe((data: Data) => {
+      this.smjerovi = data['smjerovi'];
+    });
   }
 
+  goBack() {
+    this._location.back();
+  }
 }
